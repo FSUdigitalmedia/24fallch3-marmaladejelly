5d27409d1b186168fe8852ec60f342927e98e6ac
Amanda Anderson <ada23a@fsu.edu>
2024-09-11 08:10:48 -0400

    im so lost..

--- sketch.js
+++ sketch.js
@@ -1,8 +1,14 @@
-let x = 100;     // x position of the ball
-let y = 0;       // y position of the ball
+let x1 = 100;     // x position of the ball
+let y1 = 0;       // y position of the ball
 let xSpeed1 = 3;  // speed in X direction
-let ySpeed = 7; // speed in Y direction
-let d = 20;      // diameter of the ball
+let ySpeed1 = 7; // speed in Y direction
+let d1 = 20;      // diameter of the ball
+
+let x2 = 100;     // x position of the ball
+let y2 = 0;       // y position of the ball
+let xSpeed2 = 3;  // speed in X direction
+let ySpeed2 = 7; // speed in Y direction
+let d2 = 20; 
 
 function setup() {
   createCanvas(400, 400);
@@ -13,16 +19,28 @@ function draw() {
  
   // if x hits the right side or the left side of the canvas,
   // switch directions...
-  if (x > width || x < 0) {
+  if (x1 > width || x1 < 0) {
     xSpeed1 = xSpeed1 * -2;  
   }
   x = x + xSpeed1; // adding a negative xSpeed *decreases* x, right?
   
   // same as above, but for the top and bottom of the canvas
-  if (y > width || y < 0) {
-    ySpeed = ySpeed * -2;  
+  if (y1 > width || y1 < 0) {
+    ySpeed1 = ySpeed1 * -2;  
   }
-  y = y + ySpeed;
+  y1 = y1 + ySpeed1;
 
   ellipse(x, y, d);
+   // if x hits the right side or the left side of the canvas,
+  // switch directions...
+  if (x2 > width || x2 < 0) {
+    xSpeed2 = xSpeed2 * -2;  
+  }
+  x2 = x2 + xSpeed2; // adding a negative xSpeed *decreases* x, right?
+  
+  // same as above, but for the top and bottom of the canvas
+  if (y > width || y < 0) {
+    ySpeed = ySpeed2 * -2;  
+  }
+  y2 = y2 + ySpeed2;
 }
