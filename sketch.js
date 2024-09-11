e744ec754eb16873542b4ed4920f7668eca85c62
Amanda Anderson <ada23a@fsu.edu>
2024-09-11 14:47:52 -0400

    made circle change colors

--- sketch.js
+++ sketch.js
@@ -20,31 +20,40 @@ function draw() {
 
   // if x hits the right side or the left side of the canvas,
   // switch directions...
-  if (x1 > width || x1 < 0) {
+  if (x1 > width || x1 < height) {
     xSpeed1 = xSpeed1 * -1;  
   }
-  x = x1 + xSpeed1; // adding a negative xSpeed *decreases* x, right?
+  x1 = x1 + xSpeed1; // adding a negative xSpeed *decreases* x, right?
 
   // same as above, but for the top and bottom of the canvas
   if (y1 > width || y1 < 0) {
     ySpeed1 = ySpeed1 * -1;  
   }
-  y = y1 + ySpeed1;
+  y1 = y1 + ySpeed1;
 
   // if x hits the right side or the left side of the canvas,
   // switch directions...
   if (x2 > width || x2 < 0) {
     xSpeed2 = xSpeed2 * 2;  
   }
-  x = x2 + xSpeed2; // adding a negative xSpeed *decreases* x, right?
+  x2 = x2 + xSpeed2; // adding a negative xSpeed *decreases* x, right?
 
   // same as above, but for the top and bottom of the canvas
   if (y2 > width || y2 < 0) {
     ySpeed2 = ySpeed2 * 2;  
   }
-  y = y2 + ySpeed2;
+  y2 = y2 + ySpeed2;
 
   ellipse(x1, y1, d1);
+
+  if (keyIsPressed === true) {
+    if (key === 'r') {
+      fill('red');
+    }
+  }
+
+
+
 
 
     /* when mouse button is pressed, circles turn red
